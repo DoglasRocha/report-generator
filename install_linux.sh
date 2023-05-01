d2ce3d5579ab56eaf27709e71e28d3b3cb6dd3a3
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2023-04-30 21:13:57 -0300

    generate .desktop file

--- install_linux.sh
+++ install_linux.sh
@@ -8,3 +8,14 @@ pip install -r requirements.txt
 echo "#!/bin/sh
 . ./.venv/bin/activate
 python python/gui.py" > start.sh
+
+PATH_TO_START=$(realpath start.sh)
+
+echo "[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=false
+Exec=sh $PATH_TO_START
+Name=Gerador de Relatório" > Gerador\ de\ Relatório.desktop
+
